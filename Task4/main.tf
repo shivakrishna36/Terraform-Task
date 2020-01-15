1561ed09bea181f883507349af1a0c3397193145
shivakrishna36 <45350264+shivakrishna36@users.noreply.github.com>
2020-01-15 15:52:48 +0530

    terraform lambda function

---
+++ Task4/main.tf
@@ -0,0 +1,68 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+##################################################################################
+# IAM ROLE FOR LAMBDA
+##################################################################################
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+##################################################################################
+# LAMBDA FUNCTION DEPLOY IN AWS
+##################################################################################
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "terraform-lambda.zip"
+  function_name = "terraform-lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "exports.index"
+
+################################################################################################
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+################################################################################################
+
+  source_code_hash = filebase64sha256("terraform-lambda.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
